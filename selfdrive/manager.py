ccd2838745f824ba44c52c5e89ca82a0356b9efa
Jason Young <jyoung8607@gmail.com>
2020-10-12 23:01:22 -0400

    Allow disabling of disengage-on-gas enforcement

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -561,6 +561,7 @@ def main():
     ("OpenpilotEnabledToggle", "1"),
     ("LaneChangeEnabled", "1"),
     ("IsDriverViewEnabled", "0"),
+    ("IsDisengageOnGasEnabled", "1"),
   ]
 
   # set unset params
