ef8051484cd24b958bd4d13d7fbfa18fe6743cad
Erich Moraga <33645296+ErichMoraga@users.noreply.github.com>
2020-10-24 12:25:54 -0500

    Minimized custom fork warning

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -233,7 +233,7 @@ EVENTS: Dict[int, Dict[str, Union[Alert, Callable[[Any, messaging.SubMaster, boo
       "WARNING: This branch is not tested",
       "Always keep hands on wheel and eyes on road",
       AlertStatus.userPrompt, AlertSize.mid,
-      Priority.LOWER, VisualAlert.none, AudibleAlert.none, 0., 0., 15.),
+      Priority.LOWER, VisualAlert.none, AudibleAlert.none, 0., 0., 0.),
   },
 
   EventName.startupNoControl: {
