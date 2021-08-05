57a7a65c44115fa78fbf598bce0678e55971e374
Jason Young <jyoung8607@gmail.com>
2021-08-05 19:21:09 -0400

    this ought to be interesting

--- selfdrive/car/volkswagen/interface.py
+++ selfdrive/car/volkswagen/interface.py
@@ -25,7 +25,7 @@ class CarInterface(CarInterfaceBase):
 
   @staticmethod
   def compute_gb(accel, speed):
-    return float(accel) / 3.5
+    return float(accel)
 
   @staticmethod
   def get_params(candidate, fingerprint=gen_empty_fingerprint(), car_fw=None):
