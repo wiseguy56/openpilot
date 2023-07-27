fc203efb466cce17b37f0c810440e559aa13e598
Jason Young <jyoung8607@gmail.com>
2023-07-26 21:37:35 -0400

    also change the maxes

--- selfdrive/car/volkswagen/values.py
+++ selfdrive/car/volkswagen/values.py
@@ -73,9 +73,9 @@ class CarControllerParams:
       self.LDW_STEP = 10                  # LDW_02 message frequency 10Hz
       self.ACC_HUD_STEP = 6               # ACC_02 message frequency 16Hz
       self.STEER_DRIVER_ALLOWANCE = 80    # Driver intervention threshold 0.8 Nm
-      self.STEER_MAX = 64000
-      self.STEER_DELTA_UP = 1280
-      self.STEER_DELTA_DOWN = 1280
+      self.STEER_MAX = 370
+      self.STEER_DELTA_UP = 10
+      self.STEER_DELTA_DOWN = 10
 
       if CP.transmissionType == TransmissionType.automatic:
         self.shifter_values = can_define.dv["Getriebe_11"]["GE_Fahrstufe"]
