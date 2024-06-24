285120c7541645bc4fcd6106a7dec2e76638d86b
Jason Young <jyoung8607@gmail.com>
2024-06-24 18:38:38 -0400

    Revert "add some long integral"
    This reverts commit dbe04d41ac81a745f9b23a16015f561d0e825991.

--- selfdrive/car/volkswagen/interface.py
+++ selfdrive/car/volkswagen/interface.py
@@ -85,7 +85,6 @@ class CarInterface(CarInterfaceBase):
 
     ret.pcmCruise = not ret.openpilotLongitudinalControl
     ret.longitudinalActuatorDelay = 0.4
-    ret.longitudinalTuning.kiV = [0.2]
     ret.stoppingControl = True
     ret.stopAccel = -0.55
     ret.vEgoStarting = 0.1
