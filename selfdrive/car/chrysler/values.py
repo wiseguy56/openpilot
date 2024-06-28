14f403807a0545c0e5c056766714899897878bed
wiseguy56 <wiseguy56@hotmail.com>
2024-06-27 20:47:17 -0400

    Update m/s to 15

--- selfdrive/car/chrysler/values.py
+++ selfdrive/car/chrysler/values.py
@@ -65,7 +65,7 @@ class CAR(Platforms):
   # Jeep
   JEEP_CHEROKEE_5TH_GEN = ChryslerPlatformConfig(
     [ChryslerCarDocs("Jeep Cherokee 2019-23")],
-    ChryslerCarSpecs(mass=1747., wheelbase=2.70, steerRatio=17.0, minSteerSpeed=18.5),
+    ChryslerCarSpecs(mass=1747., wheelbase=2.70, steerRatio=17.0, minSteerSpeed=15),
     dbc_dict('chrysler_cusw', None),
   )
   JEEP_GRAND_CHEROKEE = ChryslerPlatformConfig(  # includes 2017 Trailhawk
